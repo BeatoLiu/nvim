126259d405d798612fb52885eab512bf7032800c
BeatoLiu <lzf280069431@163.com>
2023-09-26 16:29:09 +0800

    init

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -113,6 +113,11 @@ local default_plugins = {
     end,
     config = function(_, opts)
       require("mason").setup(opts)
+
+      -- custom nvchad cmd to install all mason binaries listed
+      vim.api.nvim_create_user_command("MasonInstallAll", function()
+        vim.cmd("MasonInstall " .. table.concat(opts.ensure_installed, " "))
+      end, {})
     end,
   },
 
