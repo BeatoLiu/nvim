cf759c4f355b8726ba7c03ab7cc48894942a5028
BeatoLiu <lzf280069431@163.com>
2023-10-21 22:05:40 +0800

    test

--- init.lua
+++ init.lua
@@ -15,26 +15,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- which-key set
-
--- local wk = require("which-key")
--- As an example, we will create the following mappings:
--- --  * <leader>ff find files
--- --  * <leader>fr show recent files
--- --  * <leader>fb Foobar
--- -- we'll document:
--- --  * <leader>fn new file
--- --  * <leader>fe edit file
--- -- and hide <leader>1
---
-
--- wk.register({
--- ["<leader>f"] = {
--- name = "+file",
--- f = { "<cmd>Telescope find_files<cr>", "Find File" },
--- r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
--- n = { "<cmd>enew<cr>", "New File" },
--- },
--- })
-
 require("plugins")
